fc099568b7b5432d9a336f8c774e057fdce66d11
hugo <hcgar00@gmail.com>
2017-12-08 10:48:36 +0200

    Update dist

--- dist/entry.js
+++ dist/entry.js
@@ -2,7 +2,13 @@
 
 function getEntryURL(entry) {
     var url = [entry.getMeta("iconurl"), entry.getMeta("url"), entry.getMeta("loginurl")].find(Boolean);
-    return url || null;
+    if (url) {
+        if (!/^(http(s?):\/\/)/.test(url)) {
+            url = "https://" + url;
+        }
+        return url;
+    }
+    return null;
 }
 
 module.exports = {
