793a35f4ef0b070789f35cf120cc7feabd64cf49
Perry Mitchell <perry@perrymitchell.net>
2019-02-03 18:39:10 +0200

    Add index code

--- source/index.js
+++ source/index.js
@@ -1,14 +1,50 @@
-function getIconDataURI(domain) {}
+const path = require("path");
+const fs = require("fs");
+const { encode: encodeDataURI } = require("strong-data-uri");
 
-function getIconFilename(domain) {}
+function getIconDataURI(domain, opts) {
+    const readIconProcess = new Promise((resolve, reject) => {
+        const iconFilename = getIconFilename(domain, opts);
+        fs.readFile(iconFilename, (err, buff) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve(buff);
+        });
+    });
+    return readIconProcess.then(iconData => encodeDataURI(iconData, "image/png"));
+}
 
-function getIconSVG(domain) {}
+function getIconDetails(domain, { grey = false }) {
+    const { domains, match } = require("../resources/index.json");
+    const domainKey = domain.toLowerCase();
+    let filename,
+        isDefault = false;
+    if (domains[domainKey]) {
+        const { filename: imageFilename, filenameGreyscale: imageFilenameGrey } = domains[domainKey];
+        filename = grep ? imageFilenameGrey : imageFilename;
+        filename = path.resolve(__dirname, "../resources/images/", filename);
+    } else {
+        isDefault = true;
+        filename = grep ? "default.grey.png" : "default.png";
+        filename = path.resolve(__dirname, "../resources/images/", filename);
+    }
+    return {
+        filename,
+        isDefault
+    };
+}
 
-function iconExists(domain) {}
+function getIconFilename(domain, opts) {
+    return getIconDetails(domain, opts).filename;
+}
+
+function iconExists(domain) {
+    return getIconDetails(domain).isDefault === false;
+}
 
 module.exports = {
     getIconDataURI,
     getIconFilename,
-    getIconSVG,
     iconExists
 };
